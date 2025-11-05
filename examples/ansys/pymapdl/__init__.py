9fbc926293294c897a8346b607fac845b14fd9f8
andrinr <andrinrehmann@gmail.com>
2025-11-05 11:39:58 +0000

    init ansys

---
+++ examples/ansys/pymapdl/__init__.py
@@ -0,0 +1,9 @@
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+# import public API of the package
+# from . import <obj>
+
+# add public API as strings here, for example __all__ = ["obj"]
+__all__ = []
--- examples/fem-shapeopt/gf.py
+++
@@ -1,9 +0,0 @@
-import imageio
-
-images = []
-
-for i in range(60):
-    filename = f"img/mesh_optim_{i:03d}.png"
-    images.append(imageio.imread(filename))
-    print(f"Added {filename} to gif.")
-imageio.mimsave('mesh_optim.gif', images, fps=10)
